b72af258ea4bb9d0a7727ad8122803d541846098
Jaco Pretorius <jaco.pretorius@betterment.com>
2018-07-08 20:39:57 -0400

    Fix some embarassing typos

--- ext/fast_camelize/fast_camelize.c
+++ ext/fast_camelize/fast_camelize.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 static VALUE
-str_underscore(VALUE rb_string) {
+str_camelize(VALUE rb_string) {
   return rb_string;
 }
 
@@ -14,7 +14,7 @@ rb_str_camelize(VALUE self, VALUE rb_string) {
 
 void
 Init_fast_camelize(void) {
-  VALUE rb_cFastUnderscore = rb_define_module("FastCamelize");
-  rb_define_singleton_method(rb_cFastUnderscore, "camelize", rb_str_camelize, 1);
+  VALUE rb_cFastCamelize = rb_define_module("FastCamelize");
+  rb_define_singleton_method(rb_cFastCamelize, "camelize", rb_str_camelize, 1);
   rb_define_method(rb_cString, "camelize", str_camelize, 0);
 }
