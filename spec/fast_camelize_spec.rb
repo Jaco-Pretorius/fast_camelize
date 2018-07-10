4f149df78ec5a46e27a88e3f443aba53d0fe917c
Jaco Pretorius <jaco.pretorius@betterment.com>
2018-07-10 09:07:24 -0400

    Add explicit test cases around downcasing the first letter

--- spec/fast_camelize_spec.rb
+++ spec/fast_camelize_spec.rb
@@ -1,27 +1,21 @@
 require 'spec_helper'
 
 RSpec.describe FastCamelize do
-  {
-    "product"                => "product",
-    "special_guest"          => "specialGuest",
-    "application_controller" => "applicationController",
-    "area51_controller"      => "area51Controller",
-    "漢_字_😊_🎉"            => "漢字😊🎉"
-  }.each do |input, output|
-    it "camelizes #{input}" do
-      expect(FastCamelize.camelize(input, false, [], 0)).to eq output
-    end
-  end
-
-  {
+  UnderscoreToCamel = {
     "product"                => "Product",
     "special_guest"          => "SpecialGuest",
     "application_controller" => "ApplicationController",
     "area51_controller"      => "Area51Controller",
     "漢_字_😊_🎉"            => "漢字😊🎉"
-  }.each do |input, output|
-    it "camelizes #{input} and makes the first letter a capital" do
+  }
+
+  UnderscoreToCamel.each do |input, output|
+    it "camelizes #{input}" do
       expect(FastCamelize.camelize(input, true, [], 0)).to eq output
     end
+  end
+
+  it "downcases the first letter" do
+    expect(FastCamelize.camelize("Capital", false, [], 0)).to eq "capital"
   end
 end
