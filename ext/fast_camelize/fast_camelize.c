b36a0c5614f8c5b15afe78cfd6d813794f6ab9d2
Alex Mykyta <alex.mykyta@betterment.com>
2018-07-09 21:33:54 -0400

    naive implementation skipping underscores and capitalziing the next character

--- ext/fast_camelize/fast_camelize.c
+++ ext/fast_camelize/fast_camelize.c
@@ -43,14 +43,22 @@ typedef struct output_builder {
   /* } */
 /* } */
 
+output_builder_t * make_builder() {
+  return (output_builder_t *) malloc(sizeof(output_builder_t));
+}
+
+char * make_result_buffer(int len) {
+  return (char *) malloc(len * sizeof(unsigned int));
+}
+
 static VALUE
 str_camelize(VALUE self, VALUE rb_string, VALUE rb_uppercase_first_letter, VALUE rb_acronyms_array_length, VALUE rb_acronyms_array) {
 
   Check_Type(rb_string, T_STRING);
 
   //current_character_t *current_character = (current_character_t *) malloc(sizeof(current_character_t));
-  output_builder_t *builder = (output_builder_t *) malloc(sizeof(output_builder_t));
-  builder->result = (char *) malloc(RSTRING_LEN(rb_string) * sizeof(unsigned int));
+  output_builder_t *builder = make_builder();
+  builder->result = make_result_buffer(RSTRING_LEN(rb_string));
   builder->result_size = 0;
 
   rb_encoding *encoding = rb_enc_from_index(ENCODING_GET(rb_string));
@@ -58,10 +66,24 @@ str_camelize(VALUE self, VALUE rb_string, VALUE rb_uppercase_first_letter, VALUE
   char *end = RSTRING_END(rb_string);
   int current_character_size;
 
+  const int underscore = 95;
+  char capitalize = 0;
+
   while (string < end) {
     unsigned int current_character = rb_enc_codepoint_len(string, end, &current_character_size, encoding);
-    rb_enc_mbcput(current_character, &builder->result[builder->result_size], encoding);
-    builder->result_size += current_character_size;
+
+    if (capitalize) {
+      current_character = rb_enc_toupper(current_character, encoding);
+      capitalize = 0;
+    }
+
+    if (current_character == underscore) {
+      // skip and capitalize next character!
+      capitalize = 1;
+    } else {
+      rb_enc_mbcput(current_character, &builder->result[builder->result_size], encoding);
+      builder->result_size += current_character_size;
+    }
     string += current_character_size;
   }
   //builder_flush(builder);
